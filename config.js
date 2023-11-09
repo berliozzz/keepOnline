10f24b357fb4f058b5bed2ce3f7c23bd0c815e60
Nikolay Berlioz <berlioz@MacBook-Air-Nikolay.local>
2023-11-09 22:34:45 +0400

    push config to server

---
+++ config.js
@@ -0,0 +1,5 @@
+export const market = {
+  apiKey: '',
+  baseUrl: 'https://market.csgo.com/api/v2/',
+  showLog: false // если хочешь чтобы ошибки показывались в консоли - поставь тут true вместо false
+}
